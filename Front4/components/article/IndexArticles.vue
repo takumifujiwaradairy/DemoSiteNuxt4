8d6487442e80cc861958fa305b5a73621249af96
Your Name <you@example.com>
2022-02-22 15:25:15 +0900

    記事の投稿機能

---
+++ Front4/components/article/IndexArticles.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>記事一覧</h1>
+    <ul v-for="article in getArticles" :key="article.id">
+      <label>記事ID</label>
+      <li>{{ article.id }}</li>
+      <label>タイトル</label>
+      <li>{{ article.title }}</li>
+      <label>中身</label>
+      <li>{{ article.body }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+export default {
+  computed: {
+    ...mapGetters(['getArticles']),
+  },
+  methods: { 
+    ...mapActions(['fetchArticles']),
+  },
+  created () {
+    this.fetchArticles()
+  }
+}
+</script>
+
+<style>
+
+</style>
