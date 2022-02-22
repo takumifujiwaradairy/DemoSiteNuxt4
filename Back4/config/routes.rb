1e0c806f6a0fc300b8202f00bf87e95ed7d0eed1
Your Name <you@example.com>
2022-02-22 13:48:38 +0900

    ユーザーのSeedファイルの作成

--- Back4/config/routes.rb
+++ Back4/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :articles
+      resources :articles do
+        mount_devise_token_auth_for 'User', at: 'auth'
+      end
     end
   end
 end
